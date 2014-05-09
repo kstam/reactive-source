c84531b7905a391e1bbb4b24a3122cc4bbeab529
kstamatoukos <kstamatoukos@ebay.com>
2014-05-10 00:25:51 +0200

    Fixed create-test-user script so that it doesnt fail if executed twice in a row

--- reactive-source-mysql/src/test/resources/org/reactivesource/mysql/create-test-user.sql
+++ reactive-source-mysql/src/test/resources/org/reactivesource/mysql/create-test-user.sql
@@ -1,2 +1,5 @@
+GRANT USAGE ON *.* TO 'reactive_test'@'localhost';
+DROP USER 'reactive_test'@'localhost';
+
 CREATE USER 'reactive_test'@'localhost' IDENTIFIED BY 'password';
 GRANT ALL PRIVILEGES ON reactive_test.* TO 'reactive_test'@'localhost' WITH GRANT OPTION;
